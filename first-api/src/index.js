ee1d273771cd2567e44cc15c579aa8eae07eecb6
Theo Gandara <theogandarajobs@gmail.com>
2023-04-06 00:06:26 -0300

    First routes

--- first-api/src/index.js
+++ first-api/src/index.js
@@ -1,8 +1,19 @@
 const http = require('http')
+const users = require('../mocks/users')
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type':'text/html'})
-    res.end('<h1>Hello world !</h1>')
+    console.log("Method: " + req.method + ' | ' + 'Endpoint: ' + req.url )
+
+    if(req.url === '/users' && req.method === 'GET'){
+
+        const response = JSON.stringify(users)
+
+        res.writeHead(200, {'Content-Type':'application/json'})
+        res.end(response)
+    }else{
+        res.writeHead(404, {'Content-Type':'text/html'})
+        res.end(`Cannot ${req.method} ${req.url}`)
+    }
 })
 
 const PORT = 3001
